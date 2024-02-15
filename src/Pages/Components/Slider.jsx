4fb3a42a7edddd4ec9ba0bf0bff46256dee7900b
Rifat Risam <rifatrabby78@gmail.com>
2024-02-15 20:19:32 +0600

    added slider and theme change functionality

--- src/Pages/Components/Slider.jsx
+++ src/Pages/Components/Slider.jsx
@@ -1,8 +1,37 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Slider = () => {
   return (
-    <div>
-      <p className="text-2xl text-center">Slider Coming Soon</p>
-    </div>
+    <Carousel
+      showArrows={true}
+      showStatus={true}
+      showIndicators={true}
+      infiniteLoop={true}
+      showThumbs={false}
+      useKeyboardArrows={true}
+      autoPlay={true}
+      stopOnHover={true}
+      swipeable={true}
+      emulateTouch={true}
+      autoFocus={true}
+      selectedItem={0}
+      interval={2000}
+      transitionTime={900}
+      swipeScrollTolerance={5}
+    >
+      {/* First */}
+      <div className="h-[500px]">
+        <img src="https://i.ibb.co/jJzFg9c/slider.jpg" alt="" />
+      </div>
+      {/* Second */}
+      <div className="h-[500px]">
+        <img src="https://i.ibb.co/mFYr4Qv/slider-1.jpg" alt="" />
+      </div>
+      {/* Third */}
+      <div className="h-[500px]">
+        <img src="https://i.ibb.co/mSHRTCP/slider3.jpg" alt="" />
+      </div>
+    </Carousel>
   );
 };
 
